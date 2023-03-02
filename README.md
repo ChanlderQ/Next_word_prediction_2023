e6c437a17c5a3da54306c8be6b2a74733f319df3
ChanlderQ <92227847+ChanlderQ@users.noreply.github.com>
2023-03-02 09:39:40 -0500

    Add login page

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 - This is a project for Durham College Capstone 2
 - The project can predict the next word when you are typing
 
-![Image text](https://github.com/ChanlderQ/Next_word_prediction_2023/blob/main/capstone2_warmup_show.png)
--- app.py
+++ app.py
@@ -1,15 +1,66 @@
-import flask
-from flask import Flask, request, render_template
+import flask,flask_login
+from flask import Flask, g,request, render_template, Response, redirect, url_for, request, session, abort
 import json
 import main
 
-app = Flask(__name__)
+from flask_login import LoginManager, UserMixin, \
+                                login_required, login_user, logout_user
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import InputRequired,Length,ValidationError
 
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+users = []
+users.append(User(id=1, username='capstone', password='password'))
+
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
+        
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('profile'))
+
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+@app.route('/profile')
+def profile():
+    if not g.user:
+        return redirect(url_for('login'))
+
+    return render_template('profile.html')
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
-
 
 @app.route('/get_end_predictions', methods=['post'])
 def get_prediction_result():
@@ -24,6 +75,7 @@ def get_prediction_result():
         print(err)
         return app.response_class(response=json.dumps(err), status=500, mimetype='application/json')
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=8000, use_reloader=False)
+    app.run(debug=True, port=8000, use_reloader=False)
+    
+    
